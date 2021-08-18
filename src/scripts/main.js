c97bc21f49890f56c7154ab9e0ae7440c68c9bc9
dwkns <dwkns@me.com>
2021-08-18 12:49:48 +0100

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,1 @@
 import "alpinejs";
-import confetti from 'canvas-confetti';
-
-const fireworks = () =>
-  confetti.create(document.getElementById('canvas'), {
-
-  })({ particleCount: 100, spread: 100 });
-
-document.getElementById('button').addEventListener('click', () => {
-  console.log('button press');
-  fireworks();
-}); 
